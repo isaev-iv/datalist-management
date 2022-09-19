5ac068ccbb8ce366dd2c57724f80d2e70b29d496
Liam <backgroundpositioncenter@gmail.com>
2022-09-19 18:19:03 +0300

    setting up the environment

---
+++ config/build/buildResolvers.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import { ResolveOptions } from "webpack";
+
+
+export function buildResolvers(): ResolveOptions {
+
+
+    return {
+        extensions: [".tsx", ".ts", ".js"]
+    }
+    
+}
