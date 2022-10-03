73dc14f9a18f1e41d35cdb1f1fec3eba31125700
Liam <backgroundpositioncenter@gmail.com>
2022-10-04 01:24:09 +0300

    Добавил сворачиваемый Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,12 +5,18 @@ import { classNames } from "../shared/lib/classNames/classNames";
 
 import "./styles/index.scss";
 import { Navbar } from "widgets/Navbar";
+import { AppRouter } from "./providers/router";
+import { Sidebar } from "widgets/Sidebar";
 
 export const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
